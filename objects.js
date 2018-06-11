b5fd2299b9ec31e000848e0d9962be7795bcf44c
Ashwin George Philip <ashwinphilip27@gmail.com>
2018-06-11 05:11:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,5 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+  var playlist = { altJ : 'Taro'}
+  delete playlist.artistName;
   return playlist
 }
